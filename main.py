2a26d751a2dea247e18fca6241fbb503c1b7e7a3
CPMTooldev <telmnbattr@gmail.com>
2025-03-17 23:58:27 +0800

    Update and rename main (2).py to main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from cpmrm import CPMRm
+from xomboo import Xomboo
 
-__CHANNEL_USERNAME__ = "cpmrmstudio"
-__GROUP_USERNAME__   = "cpmrmstudiochat"
+__CHANNEL_USERNAME__ = "XombooChannel"
+__GROUP_USERNAME__   = "XombooChat"
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -134,7 +134,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CPMRm(acc_access_key)
+        cpm = Xomboo(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
